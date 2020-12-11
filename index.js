567a31001ce33a8a1fbabbcb34b8402ca6e8a530
EricJGardner <74524075+EricJGardner@users.noreply.github.com>
2020-12-10 21:36:53 -0600

    updated async functions
    improved async getRandomNumber function and accessing an external API for geographical locations of cities

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function getRandomNumber() {
-return newPromise(x =>
-    setTimeout(() => x(Math.floor(Math.random()*1000)), 500));
+printRandomNumber()
+    .then(() => printAllCities());
 
-    async function printRandomNumber() {
-        const randNumber = await getRandomNumber();
-    const randNumberLog = `Random Number: ${randNumber}`;    
-    console.log(randNumberLog);
-    htmlLog(randNumberLog);
-    }
-};
+async function printRandomNumber() {
+    const randNumber = await getRandomNumber();
+  const randNumberLog = `Random Number: ${randNumber}`;
+  console.log(randNumberLog);
+}
+
+async function printAllCities() {
+  for (const city of [ 'shreveport', 'dallas', 'new_york' ]) {
+    await printLatitudeAndLongitude(city);
+  }
+}
+
+function getRandomNumber() {
+    return new Promise(x =>
+      setTimeout(() => x(Math.floor(Math.random() * 1000)), 500));
+}
+
+async function printLatitudeAndLongitude(city) {
+    const url = `https://geocode.xyz/${city}?json=1`;
+  
+  let res = await fetch(url);
+  
+  if (res.status != 200) {
+    res = await new Promise(x => setTimeout(async () => {
+        const tmpRes = await fetch(url);
+        x(tmpRes);
+      }, 1000));
+  }
+
+    const json = await res.json();
+  const output = `${json.standard.city} - Latitude: ${json.latt}, Longitude: ${json.longt}`;
+  console.log(output);
+}
+
